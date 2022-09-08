67bf0c8b2f758ee06965e7a5702aeb2dae62e997
latman09 <lateefsulaiman09@gmail.com>
2022-09-08 07:05:45 -0700

    new task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - entry point
+ * 
+ * return 0 after printing the functon
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
